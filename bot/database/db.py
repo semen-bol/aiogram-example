08d8a3a7dbe17ec6aaae4753512acd39d430765d
NemoDev <82635729+semen-bol@users.noreply.github.com>
2024-10-12 04:04:50 +0500

    Update db.py

--- bot/database/db.py
+++ bot/database/db.py
@@ -8,7 +8,7 @@ import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 
 class db:
-    def __init__(self, host: str, username: str, password: str, database: str, isLocal: bool) -> None:
+    def __init__(self, host: str, username: str, password: str, database: str, isLocal: bool = False) -> None:
         self._host = host
         self._username = username
         self._password = password
@@ -16,10 +16,7 @@ class db:
 
         self.client = None
         self.db = None
-        if isLocal == False:
-            self._connect(False)
-        elif isLocal == True:
-            self._connect(True)
+        self._connect(isLocal)
 
     def _connect(self, isLocal):
         if isLocal == False:
