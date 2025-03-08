d509f3d745503601b904565ebe2f973fa9362ea7
semen-bol <syoma.bolotov@bk.ru>
2025-03-09 01:41:27 +0500

    -

--- main.py
+++ main.py
@@ -45,4 +45,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.debug("Exiting...")
